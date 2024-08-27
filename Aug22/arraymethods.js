336e124ca96259a46aa0b363e185254ff80154f6
Suvdaa1001 <suvdaa.emam@gmail.com>
2024-08-26 22:46:43 -0400

    array methods

---
+++ Aug22/arraymethods.js
@@ -0,0 +1,13 @@
+// const array =[1,2,3,4,5];
+// array.reverse();
+// console.log (array);
+
+// const array = [2,3,1,0,4];
+// array.sort();
+// console.log(array);
+
+// const fruits = ["banana", "apple", "orange", "mango"];
+// console.log(fruits.toString);
+
+const array = [100, 200];
+console.log(array.includes(100));
---
+++ Aug22/index.js
@@ -0,0 +1,32 @@
+// const array = [1,2,3,4,5];
+// let result = [];
+// 1. elementeern araas ni guine
+// 2. shine array 
+// 3. shine array ruu neg negeer ni urvuulj gargsn toonuudaa oruulna
+
+// for(let index = array.lenght -1; index >= 0; i--){
+//   console.log(array[index]);
+
+//   console.log(result.length, "resu;t.lemght");
+//   debugger;
+// result [0] = array[index];
+
+// }
+// console.log(result)
+
+// const array = [1,2,3];
+// array.push(4);
+// console.log(array);
+
+// const array = [1,2,3];
+// let num = array.pop();
+// //array.pop();
+// //array.pop();
+// console.log(array);
+
+var greeting = "hey hi";
+var count = 4;
+if (count > 3) {
+  var greeting = " say Hello instead";
+}
+console.log(greeting);
